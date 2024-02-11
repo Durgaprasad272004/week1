1910097b19ecd3ec12e1f5046651e7ca77a872a2
Durgaprasad272004 <158829667+Durgaprasad272004@users.noreply.github.com>
2024-02-11 17:43:14 +0530

    Add files via upload

---
+++ Helloname.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Helloname1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter user name");
+        String name=sc.next();
+        System.out.println("Hello"+" "+name);
+    }
+}
